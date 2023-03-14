7d17fbcd7300588ff9a451687706baa3d48b45ac
Alissa Wiley <alissa.estelle.w@gmail.com>
2023-03-14 11:17:34 -0400

    Created Factories

--- app/Models/Author.php
+++ app/Models/Author.php
@@ -7,7 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Author extends Model
 {
+    // L:11 is usually implicit, but has been written explicitly for this demo.
     protected $table = 'authors';
+    protected $fillable = [
+        'name',
+        'title',
+        'company',
+        'email'
+    ];
+
     public function books() {
         return $this->hasMany(Book::class);
     }
--- app/Models/Book.php
+++ app/Models/Book.php
@@ -7,6 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
+    // L:11 is usually implicit, but has been written explicitly for this demo.
     protected $table = 'books';
+    protected $fillable = [
+        'title',
+        'author_id',
+        'abstract'
+    ];
+    public function author() {
+        return $this->belongsTo(Author::class);
+    }
     use HasFactory;
 }
